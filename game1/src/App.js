bde1e98215a425be4463dce923f1ea6e78c9cb46
TNSHR <rohandw0@gmail.com>
2025-01-13 22:09:57 +0530

    game1

---
+++ game1/src/App.js
@@ -0,0 +1,15 @@
+
+
+import { Component } from 'react';
+import './App.css';
+import Game from './components/game.js';
+
+class App extends Component{  
+    render(){
+      return (
+      <Game/>
+    );
+  }
+}
+
+export default App;
